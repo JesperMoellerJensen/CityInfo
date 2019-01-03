9d3c1ef9a20d2b8890477017cbc30f05227f75ad
Unknown <jesp626a@edu.eal.dk>
2019-01-03 16:52:31 +0100

    d

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,42 +1,52 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CityInfo.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
 {
-    [Route("api/cities")]
-    public class PointsOfInterestController : Controller
-    {
-        [HttpGet("{cityId}/pointsofinterest")]
-        public IActionResult GetPointsOfInterest(int cityId)
-        {
-            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
-            if (city == null)
-            {
-                return NotFound();
-            }
+	[Route("api/cities")]
+	public class PointsOfInterestController : Controller
+	{
+		[HttpGet("{cityId}/pointsofinterest")]
+		public IActionResult GetPointsOfInterest(int cityId)
+		{
+			var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+			if (city == null)
+			{
+				return NotFound();
+			}
 
-            return Ok(city.PointsOfInterest);
-        }
+			return Ok(city.PointsOfInterest);
+		}
 
-        [HttpGet("{cityId}/pointsofinterest/{id}")]
-        public IActionResult GetPointOfInterest(int cityId, int id)
-        {
-            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
-            if (city == null)
-            {
-                return NotFound();
-            }
+		[HttpGet("{cityId}/pointsofinterest/{id}")]
+		public IActionResult GetPointOfInterest(int cityId, int id)
+		{
+			var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+			if (city == null)
+			{
+				return NotFound(); 
+			}
 
-            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
-            if (pointOfInterest == null)
-            {
-                return NotFound();
-            }
+			var pointOfInterest = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+			if (pointOfInterest == null)
+			{
+				return NotFound();
+			}
 
-            return Ok(pointOfInterest);
-        }
-    }
+			return Ok(pointOfInterest);
+		}
+
+		[HttpPost("{cityid}/pointofinterest")]
+		public IActionResult CreatePointOfInterest(int cityId, [FromBody] PointOfInterestForCreationDto pointOfInterest)
+		{
+			if(pointOfInterest == null)
+			{
+				return BadRequest();
+			}
+		}
+	}
 }
